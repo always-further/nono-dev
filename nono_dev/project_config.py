03a59bce99f7d2e9b515322d14f1eabbf075f36e
Luke Hinds <lukehinds@gmail.com>
2026-05-12 07:42:00 +0100

    feat(config): support tilde in worktree dir config
    This change allows the `worktree.dir` configuration value to use `~` as a shorthand for the user's home directory. This improves convenience and portability by letting users specify worktree paths relative to their home directory without hardcoding absolute paths.
    
    Signed-off-by: Luke Hinds <lukehinds@gmail.com>

--- nono_dev/project_config.py
+++ nono_dev/project_config.py
@@ -152,7 +152,7 @@ def get_worktree_dir(config):
     repo name is auto-appended to keep worktrees from different
     projects separated (e.g. /Volumes/SSD/worktrees/nono/).
     """
-    wt_dir = config["worktree"]["dir"]
+    wt_dir = os.path.expanduser(config["worktree"]["dir"])
     if os.path.isabs(wt_dir):
         if config.get("_worktree_source") == "user":
             repo = get_repo(config)
